6db91798ee97f7379d9bebbfd0f779a8874c3c86
mcheung008 <62771803+mcheung008@users.noreply.github.com>
2024-03-16 21:04:20 -0700

    Modified main, css, and js.

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
     const contactForm = document.querySelector("#contact form");
     contactForm.addEventListener("submit", function(event) {
-        event.preventDefault(); // Prevent the form from submitting
+        event.preventDefault(); 
+
+   
     });
+
+
+    const toggleDarkMode = () => {
+        document.body.classList.toggle('dark-mode');
+    };
+
+
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+    if (darkModeToggle) {
+        darkModeToggle.addEventListener('click', toggleDarkMode);
+    }
 });
